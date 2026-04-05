a8636d244483c752c861bce2a2741d2145cde90a
KhanhSki <khanh0193066@gmail.com>
2026-04-05 14:26:46 +0700

    Setup header and font

--- fe/liqi/src/pages/users/theme/header/index.js
+++ fe/liqi/src/pages/users/theme/header/index.js
@@ -1,7 +1,54 @@
+
 import { memo } from 'react';
+import "./style.scss"
+import { Link } from 'react-router-dom'
 
 const Header = () =>{
-    return <h1>Header</h1>
+    return <div className="header__top">
+        <div className='row'>
+            <div className='col-6 header__top_left'>
+                <ul>
+                    <li>
+                        <Link to={""} >
+                            LOGO
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to={""} >
+                            Giới Thiệu
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to={""} >
+                            Cửa Hàng
+                        </Link>
+                    </li>
+
+                    <li>
+                        <Link to={""} >
+                            Dịch Vụ
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+            <div className='col-6 header__top_right'>
+                <ul>
+                    <li>
+                        <Link to={""} >
+                            Đăng Nhập
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={""} >
+                            Đăng Ký
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
 }
 
 export default memo(Header);
