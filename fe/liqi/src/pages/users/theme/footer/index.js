12f5678bd1099a368d5e5232a3b65ecf632c1a9b
KhanhSki <khanh0193066@gmail.com>
2026-04-12 15:01:25 +0700

    Done code header and footer

--- fe/liqi/src/pages/users/theme/footer/index.js
+++ fe/liqi/src/pages/users/theme/footer/index.js
@@ -6,42 +6,46 @@ import { SiZalo } from "react-icons/si";
 
 const Footer = () =>{
     return (
-        <div className='container wide footer__bot'>
+        <footer className='container wide footer__bot'>
             <div className='row'>
-                <div className='col lg-4'>
-                    <div className='row footer__left'>
-                        <div className='col lg-12 logo'>LOGO</div>
-                        <div className='col lg-12 social'>
-                            <ul className='row'>
-                                <li className='col lg-1'>
+                <div className='col lg-4 md-4 lmd-12 sm-12'>
+                    <div className='row lmd-jcenter sm-jcenter footer__left'>
+                        <div className='col lg-3 md-3 lmd-3 sm-3  logo'>LOGO</div>
+                        <div className='col lg-12 md-12 lmd-12 sm-12 social'>
+                            <ul className='row lmd-jcenter sm-jcenter'>
+                                <li className='col lg-1 md-1 lmd-1 sm-2'>
                                     <Link to=""> <FaFacebook /> </Link>
                                 </li>
 
-                                <li className='col lg-1'>
+                                <li className='col lg-1 md-1 lmd-1 sm-2'>
                                     <Link to=""> <FaTiktok/> </Link>
                                 </li>
 
-                                <li className='col lg-1'>
+                                <li className='col lg-1 md-1 lmd-1 sm-2'>
                                     <Link to=""> <SiZalo/> </Link>
                                 </li>
 
-                                <li className='col lg-1'>
+                                <li className='col lg-1 md-1 lmd-1 sm-2'>
                                     <Link to=""> <FaYoutube/> </Link>
                                 </li>
 
-                                <li className='col lg-1'>
+                                <li className='col lg-1 md-1 lmd-1 sm-2'>
                                     <Link to=""> <FaInstagram/> </Link>
                                 </li>
                                 
                             </ul>
                         </div>
-                        <div className='col lg-12 para'>prod@0.1.1 <br/>© 2026 LiQi Studio, LLC</div>
+                        <div className='col lg-3 md-3 lmd-3 sm-3'>
+                            <p>
+                                prod@0.1.1 <br/>© 2026 KhanhSki
+                            </p>
+                        </div>
                     </div>
                 </div>
-                <div className='col lg-8'>
-                    <div className='row footer__right'>
+                <div className='col lg-8 md-8 lmd-12 sm-12'>
+                    <div className='row footer__right lmd-jcenter sm-jcenter'>
 
-                        <div className='col lg-4'>
+                        <div className='col lg-4 md-4 lmd-4 sm-12'>
                             <span className='title'>Công Ty</span>
                             <ul>
                                 <li>
@@ -58,7 +62,7 @@ const Footer = () =>{
                             </ul>
                         </div>
 
-                        <div className='col lg-4'>
+                        <div className='col lg-4 md-4 lmd-4 sm-12'>
                             <span className='title'>Luật</span>
                             <ul>
                                 <li>
@@ -71,7 +75,7 @@ const Footer = () =>{
                             </ul>
                         </div>
                 
-                        <div className='col lg-4'>
+                        <div className='col lg-4 md-4 lmd-4 sm-12'>
                             <span className='title'>Liên Hệ</span>
                             <ul>
                                 <li>
@@ -91,8 +95,15 @@ const Footer = () =>{
 
                     </div>
                 </div>
+
+                <div className='footer__bot__about lg-12 md-12 lmd-12 sm-12 '>
+                    <p>LiQi không liên kết với bất kỳ trò chơi hoặc công ty trò chơi nào. Tất cả sản phẩm ở đây đều được chọn lọc và được bảo kê chọn đời.</p>
+                    <p>Shop LiQi Bán Acc Liên Quân Uy tín - Giá Rẻ - An toàn - Tự động. Ở đây có bán acc, thu acc, nạp game, trả góp, giao lưu lên đời, tìm acc theo yêu cầu...</p>
+                </div>
             </div>
-        </div>
+
+            
+        </footer>
     )
 }
 
