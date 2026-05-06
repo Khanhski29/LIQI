a936f085c7c17892bd458c8803a57d5734de5e84
KhanhSki <khanh0193066@gmail.com>
2026-05-06 19:30:19 +0700

    Done responsive Product Detail and Service Page

--- fe/liqi/src/pages/users/servicePage/index.js
+++ fe/liqi/src/pages/users/servicePage/index.js
@@ -16,25 +16,25 @@ const ServicePage = () => {
             <Title name="Dịch Vụ Chu Đáo" />
 
             <div className="service__container row">
-                <div className="service__item col lg-4">
+                <div className="service__item col lg-4 md-4 lmd-12 sm-12">
                     <p className="service__title title__sell">Bạn Muốn Bán Acc</p>
                     <p>+ thu mua acc giá cao</p>
                     <p>+ cân thông tin xấu</p>
                 </div>
 
-                <div className="service__item col lg-4">
+                <div className="service__item col lg-4 md-4 lmd-12 sm-12">
                     <p className="service__title title__order">Order Acc Theo Yêu Cầu</p>
                     <p>+ tìm acc như ý muốn</p>
                     <p>+ không yêu cầu nào làm khó được shop</p>
                 </div>
 
-                <div className="service__item col lg-4">
+                <div className="service__item col lg-4 md-4 lmd-12 sm-12">
                     <p className="service__title title__trans">Tôi Muốn Giao Dịch Trực Tiếp</p>
                     <p>+ gặp tận nơi để đảm bảo khi acc giá trị cao</p>
                     <p>+ đảm bảo sự uy tín trọn vẹn</p>
                 </div>
 
-                <div className="service__about col lg-12">
+                <div className="service__about col lg-12 md-12 lmd-12 sm-12">
                     <p>
                         Chúng tôi mang đến dịch vụ mua bán và order acc game một cách nhanh chóng, uy tín và đáng tin cậy, giúp bạn tiết kiệm thời gian mà vẫn sở hữu được tài khoản như mong muốn. Không chỉ hỗ trợ thu mua acc với mức giá hấp dẫn, chúng tôi còn sẵn sàng tìm kiếm những tài khoản phù hợp theo yêu cầu riêng của từng khách hàng, từ phổ thông đến hiếm, giá trị cao. Đối với các giao dịch lớn, bạn có thể hoàn toàn yên tâm khi được hỗ trợ giao dịch trực tiếp với quy trình rõ ràng, minh bạch. Mỗi bước đều được kiểm tra kỹ lưỡng nhằm đảm bảo an toàn tuyệt đối, hạn chế rủi ro và bảo vệ tối đa quyền lợi của bạn. Sự hài lòng và tin tưởng của khách hàng chính là điều mà chúng tôi luôn đặt lên hàng đầu, và cũng là động lực để không ngừng nâng cao chất lượng dịch vụ mỗi ngày.
                     </p>
